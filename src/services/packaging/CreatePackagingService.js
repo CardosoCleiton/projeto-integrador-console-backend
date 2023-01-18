6f7e0c3245fffc4fef365044de35d2857ac47a5d
Aline Alegre <alinealegre@icloud.com>
2023-01-17 21:15:06 -0300

    Inserção do cálculo do pacote dos correios

--- src/services/packaging/CreatePackagingService.js
+++ src/services/packaging/CreatePackagingService.js
@@ -11,6 +11,22 @@ class CreatePackagingService{
          }
       });
 
+      if(properties.width < 10 || properties.width > 100){
+         throw new AppError("A largura mínima do pacote é de 10cm e a máxima é de 100cm");
+      }
+
+      if(properties.length < 15 || properties.length > 100){
+         throw new AppError("O comprimento mínimo do pacote é de 15cm e o máximo é de 100cm");
+      }
+
+      if(properties.height < 1 || properties.height > 100){
+         throw new AppError("A altura mínima do pacote é de 1cm e a máxima é de 100cm");
+      }
+
+      if (properties.width + properties.length + properties.height > 200 ) {
+      throw new AppError("A soma resultante do comprimento + largura + altura não deve superar 200cm");
+      }
+
       const id = uuidV4();
 
       const packaging = await Packaging.create({
